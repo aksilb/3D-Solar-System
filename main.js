a5da363ebc588e714112191e325672955c529484
Abdelghani Benhaoui <66739622+aksilb@users.noreply.github.com>
2020-07-19 17:34:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
-// window.sr = scrollBy();
+$(window).load(function(){
 
-// scrollBy.apply(".animate-right", {
-//     origin: "left",
-//     duration: 1000,
-//     distance: "24rem",
-//     delay: 300,
-// });
-// sr.reveal(".animate-right", {
-//     origin: "right",
-//     duration: 1000,
-//     distance: "24rem",
-//     delay: 600,
-// });
-// sr.reveal(".animate-top", {
-//     origin: "top",
-//     duration: 1000,
-//     distance: "24rem",
-//     delay: 600,
-// });
-// sr.reveal(".animate-bottom", {
-//     origin: "bottom",
-//     duration: 1000,
-//     distance: "24rem",
-//     delay: 600,
-// });
+  var body = $("body"),
+      universe = $("#universe"),
+      solarsys = $("#solar-system");
+
+  var init = function() {
+    body.removeClass('view-2D opening').addClass("view-3D").delay(2000).queue(function() {
+      $(this).removeClass('hide-UI').addClass("set-speed");
+      $(this).dequeue();
+    });
+  };
+
+  var setView = function(view) { universe.removeClass().addClass(view); };
+
+  $("#toggle-data").click(function(e) {
+    body.toggleClass("data-open data-close");
+    e.preventDefault();
+  });
+
+  $("#toggle-controls").click(function(e) {
+    body.toggleClass("controls-open controls-close");
+    e.preventDefault();
+  });
+
+  $("#data a").click(function(e) {
+    var ref = $(this).attr("class");
+    solarsys.removeClass().addClass(ref);
+    $(this).parent().find('a').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
+  $(".set-zoom").click(function() { body.toggleClass("zoom-large zoom-close"); });
+  $(".set-speed").click(function() { setView("scale-stretched set-speed"); });
+  $(".set-size").click(function() { setView("scale-s set-size"); });
+  $(".set-distance").click(function() { setView("scale-d set-distance"); });
+
+  init();
+
+});
